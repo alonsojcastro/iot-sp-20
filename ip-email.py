c1c59daaa302218dad15f935f7216983579eb192
Alonso <13617669+alonsojcastro@users.noreply.github.com>
2020-02-04 20:40:57 -0500

    updating ip-email
    sending your RPi IP address to your email on startup.

--- ip-email.py
+++ ip-email.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/env python
 # modified from http://elinux.org/RPi_Email_IP_On_Boot_Debian
 # modified from Don Southard
@@ -11,37 +12,37 @@ import urllib2
 # Change to your own account information
 tries = 0
 while True:
-	if (tries>10):
-		exit()
-	try:
+        if (tries>10):
+                exit()
+        try:
 
-		user_name = "your name"
-		to = 'your-email'
-		gmail_user = 'same-email'
-		gmail_password = 'email-password'
-		smtpserver = smtplib.SMTP('smtp.gmail.com', 587)
-		smtpserver.ehlo()
-		smtpserver.starttls()
-		smtpserver.ehlo
-		smtpserver.login(gmail_user, gmail_password)
-		today = datetime.date.today()
-		# Very Linux Specific
-		arg='ip route list'
-		p=subprocess.Popen(arg,shell=True,stdout=subprocess.PIPE)
-		data = p.communicate()
-		split_data = data[0].split()
-		ipaddr = split_data[split_data.index('src')+1]
-		extipaddr = urllib2.urlopen("http://icanhazip.com").read()
-		my_ip = 'Local address: %s\nExternal address: %s' %  (ipaddr, extipaddr)
-		msg = MIMEText(my_ip)
-		msg['Subject'] = user_name +' the IP for your RaspberryPi on %s' % today.strftime('%b %d %Y')
-		msg['From'] = gmail_user
-		msg['To'] = to
-		#time.sleep(5)
-		smtpserver.sendmail(gmail_user, [to], msg.as_string())
-		smtpserver.quit()
-		break
-	except Exception as e:
-		tries = tries + 1
-		time.sleep(1)
+                user_name = "your_name"
+                to = 'add-your-email@youremail.com'
+                gmail_user = 'add-your-email@youremail.com'
+                gmail_password = 'add-your-password'
+                smtpserver = smtplib.SMTP('smtp.gmail.com', 587)
+                smtpserver.ehlo()
+                smtpserver.starttls()
+                smtpserver.ehlo
+                smtpserver.login(gmail_user, gmail_password)
+                today = datetime.date.today()
+                # Very Linux Specific
+                arg='ip route list'
+                p=subprocess.Popen(arg,shell=True,stdout=subprocess.PIPE)
+                data = p.communicate()
+                split_data = data[0].split()
+                ipaddr = split_data[split_data.index('src')+1]
+                extipaddr = urllib2.urlopen("http://icanhazip.com").read()
+                my_ip = 'Local address: %s\nExternal address: %s' %  (ipaddr, exti$
+                msg = MIMEText(my_ip)
+                msg['Subject'] = user_name +' the IP for your RaspberryPi on %s' %$
+                msg['From'] = gmail_user
+                msg['To'] = to
+                #time.sleep(5)
+                smtpserver.sendmail(gmail_user, [to], msg.as_string())
+                smtpserver.quit()
+                break
+        except Exception as e:
+                tries = tries + 1
+                time.sleep(1)
 

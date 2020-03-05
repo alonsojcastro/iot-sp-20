6a83af9599d7dbab786afe3f2e1210e0df1ab269
Alonso <13617669+alonsojcastro@users.noreply.github.com>
2020-03-04 23:05:19 -0500

    procon serial arduino

---
+++ proConSerial.ino
@@ -0,0 +1,16 @@
+String inputString = "";         // a string to hold incoming data
+boolean stringComplete = false;  // whether the string is complete
+
+void setup() {
+  // initialize serial:
+  Serial.begin(9600);
+  // reserve 200 bytes for the inputString:
+  inputString.reserve(200);
+}
+
+void loop() {
+
+    Serial.println(random(0,255));
+    delay(1000);
+
+}

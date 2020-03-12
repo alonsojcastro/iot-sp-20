6f62e01d9f24d7517bcacec16fde83497768710b
Alonso <13617669+alonsojcastro@users.noreply.github.com>
2020-03-11 22:04:03 -0400

    updating read + write

--- textReadWrite.js
+++ textReadWrite.js
@@ -1,10 +1,10 @@
 var fs = require("fs");
-/////READING
+///READING
 fs.readFile("temp.txt", function(err, buf) {
   console.log(buf.toString());
 });
 
-//////Handling Errors
+//////Handling Errors - you can call the file whatever you want but make sure you always look for it and have an error handler
 // fs.readFile("not-found.txt", "utf-8", (err, data) => {
 //     if (err) { console.log(err) }
 //     console.log(data);
@@ -12,9 +12,23 @@ fs.readFile("temp.txt", function(err, buf) {
 
 
 /////WRITING
-// var data = "New File Contents";
-
+// var data = "Adding some content to this file and this is my poem";
+//
 // fs.writeFile("temp.txt", data, (err) => {
 //   if (err) console.log(err);
 //   console.log("Successfully Written to File.");
 // });
+
+// 'a' means appending (old data will be preserved)
+// var logger = fs.createWriteStream('temp.txt', {
+//   flags: 'a'
+// })
+//
+// // append string to your file
+// logger.write('' + 'some data' + '')
+//
+// // again
+// logger.write('more data')
+//
+// // again
+// logger.write('and more')

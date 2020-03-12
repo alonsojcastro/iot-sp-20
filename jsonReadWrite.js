cf521476c2dcf074afd4e7e75c8399211c4c5af4
Alonso <13617669+alonsojcastro@users.noreply.github.com>
2020-03-11 23:59:49 -0400

    Update and rename JSONReadWrite.js to jsonReadWrite.js

--- jsonReadWrite.js
+++ jsonReadWrite.js
@@ -1,13 +1,13 @@
 var fs = require("fs");
 var Request = require("request");
 /////READING
-// let rawdata = fs.readFileSync('myjson.json');  
-// let student = JSON.parse(rawdata);  
+// let rawdata = fs.readFileSync('myjson.json');
+// let student = JSON.parse(rawdata);
 // console.log(student);
 
 
 //////fs.readFile
-// fs.readFile('myjson.json', (err, data) => {  
+// fs.readFile('myjson.json', (err, data) => {
 //     if (err) throw err;
 //     let student = JSON.parse(data);
 //     console.log(student);
@@ -17,7 +17,7 @@ var Request = require("request");
 
 
 ///////REMOTE REQUEST
-// Request.get("http://ayo.io/myjson.json", (error, response, body) => {
+// Request.get("https://alonsojcastro.github.io/iot-sp-20/docs/myjson.json", (error, response, body) => {
 //     if(error) {
 //         return console.dir(error);
 //     }
@@ -27,32 +27,32 @@ var Request = require("request");
 
 
 /////WRITING
-// let student = {  
+// let student = {
 //     name: 'Mike',
-//     age: 23, 
+//     age: 23,
 //     pet: 'lobster',
 //     department: 'English',
-//     car: 'Honda' 
+//     car: 'Honda'
 // };
 
-// let data = JSON.stringify(student);  
+// let data = JSON.stringify(student);
 // fs.writeFileSync('out.json', data);
 
 
 //////Using fs.writeFile
-// let student = {  
+// let student = {
 //     name: 'Mike',
-//     age: 23, 
+//     age: 23,
 //     pet: 'flea',
 //     department: 'English',
-//     car: 'Honda' 
+//     car: 'Honda'
 // };
 
 // let data = JSON.stringify(student, null, 2);
 
-// fs.writeFile('out.json', data, (err) => {  
+// fs.writeFile('out.json', data, (err) => {
 //     if (err) throw err;
 //     console.log('Data written to file');
 // });
 
-// console.log('This is after the write call');  
+// console.log('This is after the write call');

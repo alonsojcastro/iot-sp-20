e890d905a003aa25920c6d4c167f760ddf259817
Alonso <13617669+alonsojcastro@users.noreply.github.com>
2020-04-07 21:26:23 -0400

    Update myexpress.js

--- myexpress.js
+++ myexpress.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var app = express();
+
 app.get('/', function (req, res) {
-  res.send('SACRA COLLECTIVE!'); });
+  res.send("Welcome to Studio X"); });
+
 var server = app.listen(8000, function () {
   var host = server.address().address;
   var port = server.address().port;
-  console.log('Example app listening at http://%s:%s', host, port);
+  console.log('This is an example App listening at http://%s:%s', host, port);
 });

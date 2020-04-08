61c75520df80b35041ddffbbed174352ea79b1c4
Alonso <13617669+alonsojcastro@users.noreply.github.com>
2020-04-07 21:52:51 -0400

    updating files

--- expressdir.js
+++ expressdir.js
@@ -1,17 +1,13 @@
-
 var express = require('express');
 var path=require('path');
 var app = express();
 var url = "localhost"
-var port = 8080
+var port = 8000
 
 app.use('/',express.static(path.join(__dirname,'public')));
 
-
-app.listen(port);
-console.log(url+" listening on port "+ port)
-
-
-
 app.get('/test', function (req, res) {
-  res.send('SACRA COLLECTIVE!'); });
+  res.send('Studio X!'); });
+
+  app.listen(port);
+  console.log(url+" listening on port "+ port)
